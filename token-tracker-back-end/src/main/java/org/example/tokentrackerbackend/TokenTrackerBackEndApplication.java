2101635ecc5a71f6e211f255bf1dcaa3072d6731
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-01-24 17:13:34 -0600

    first commit

---
+++ token-tracker-back-end/src/main/java/org/example/tokentrackerbackend/TokenTrackerBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.tokentrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenTrackerBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenTrackerBackEndApplication.class, args);
+	}
+
+}
---
+++ token-tracker-back-end/src/test/java/org/example/tokentrackerbackend/TokenTrackerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tokentrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenTrackerBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
