9dd0f99937112b4ef0619cf47d865cbd8f553789
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-02-11 01:13:57 -0500

    updated interfaces

--- token-tracker-front-end/src/types/types.ts
+++ token-tracker-front-end/src/types/types.ts
@@ -9,7 +9,6 @@ export interface TokenSearchResult {
     imageUri?: string;
     artist?: string;
 }
-
 export interface TokenType {
     oracleId: string;
     side: number;
@@ -21,17 +20,43 @@ export interface TokenType {
     art?: string;
     artist?: string;
 }
-
 export interface TokenPaletteType {
     id: string;
     name: string;
     tokens: TokenType[];
 }
-
-
 export interface Token {
     id: string;
     name: string;
-    count: number;
     tokenType: TokenType;
+}
+export interface GameInstance {
+    id: string;
+    tokenPalette: TokenPaletteType;
+    createdAt: string;
+    updatedAt: string;
+    status: "ACTIVE" | "ENDED";
+}
+export interface GameToken {
+    id: string;
+    gameInstance: GameInstance;
+    tokenType: TokenType;
+    isSick: boolean;
+    isTapped: boolean;
+    counters: number;
+}
+export interface TokenIdentifyingPair {
+    oracleId: string;
+    side: number;
+}
+export interface TokenDetails {
+    oracleId: string;
+    side: number;
+    name: string;
+    typeLine: string;
+    oracleText?: string;
+    power?: string;
+    toughness?: string;
+    imageUri?: string;
+    artist?: string;
 }
