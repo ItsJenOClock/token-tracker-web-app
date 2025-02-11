593a84677bd7140ac7b63530f7b016c410ac10a6
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-02-11 00:34:53 -0500

    added findByTokenPalette and findByTokenPaletteId in repository of token

--- token-tracker-back-end/src/main/java/org/example/tokentrackerbackend/repositories/TokenRepository.java
+++ token-tracker-back-end/src/main/java/org/example/tokentrackerbackend/repositories/TokenRepository.java
@@ -1,9 +1,14 @@
 package org.example.tokentrackerbackend.repositories;
 
-import org.example.tokentrackerbackend.models.Token;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.example.tokentrackerbackend.models.Token;
+import org.example.tokentrackerbackend.models.TokenPalette;
+import java.util.List;
 
-@Repository
-public interface TokenRepository extends JpaRepository<Token, Long> {
+@Transactional
+public interface TokenRepository extends JpaRepository < Token, Long > {
+    List < Token > findByTokenPalette(TokenPalette tokenPalette);
+
+    List < Token > findByTokenPaletteId(Long paletteId);
 }
