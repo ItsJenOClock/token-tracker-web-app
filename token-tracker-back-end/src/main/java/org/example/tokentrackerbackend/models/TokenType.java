33311e90cc022646ffcc4aae8d282c424f2e421c
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-02-11 00:28:28 -0500

    reformatted, implemented getOracleId and getSide

--- token-tracker-back-end/src/main/java/org/example/tokentrackerbackend/models/TokenType.java
+++ token-tracker-back-end/src/main/java/org/example/tokentrackerbackend/models/TokenType.java
@@ -15,20 +15,21 @@ public class TokenType {
     private String type;
     private String text;
     private String art;
+    private String oracleId;
+    private int side;
 
     @ManyToOne
     @JoinColumn(name = "scryfall_api_id", nullable = false)
     @JsonIgnore
     private ScryfallApi scryfallApi;
-
+    
     @JsonIgnore
     @OneToMany(mappedBy = "tokenType", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Token> tokens;
+    private List < Token > tokens;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -36,7 +37,6 @@ public class TokenType {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -44,7 +44,6 @@ public class TokenType {
     public String getType() {
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
@@ -52,7 +51,6 @@ public class TokenType {
     public String getText() {
         return text;
     }
-
     public void setText(String text) {
         this.text = text;
     }
@@ -60,20 +58,37 @@ public class TokenType {
     public String getArt() {
         return art;
     }
-
     public void setArt(String art) {
         this.art = art;
+    }
+
+
+    public String getOracleId() {
+        if (oracleId == null && scryfallApi != null) {
+            return scryfallApi.getOracleId();
+        }
+        return oracleId;
+    }
+    public void setOracleId(String oracleId) {
+        this.oracleId = oracleId;
+    }
+
+
+    public int getSide() {
+        return side;
+    }
+    public void setSide(int side) {
+        this.side = side;
     }
 
     public ScryfallApi getScryfallApi() {
         return scryfallApi;
     }
-
     public void setScryfallApi(ScryfallApi scryfallApi) {
         this.scryfallApi = scryfallApi;
     }
 
-    public List<Token> getTokens() {
+    public List < Token > getTokens() {
         return tokens;
     }
 }
