7036a1df31e2d73143a57d8a3a499a09cf726c36
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-02-11 00:35:17 -0500

    reformatted scryfall api repository

--- token-tracker-back-end/src/main/java/org/example/tokentrackerbackend/repositories/ScryfallApiRepository.java
+++ token-tracker-back-end/src/main/java/org/example/tokentrackerbackend/repositories/ScryfallApiRepository.java
@@ -3,10 +3,11 @@ package org.example.tokentrackerbackend.repositories;
 import org.example.tokentrackerbackend.models.ScryfallApi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
-public interface ScryfallApiRepository extends JpaRepository<ScryfallApi, Integer> {
-    List<ScryfallApi> findByName(String name);
+public interface ScryfallApiRepository extends JpaRepository < ScryfallApi, Integer > {
+    List < ScryfallApi > findByNameContainingIgnoreCase(String name);
     ScryfallApi findByOracleIdAndSide(String oracleId, int side);
 }
