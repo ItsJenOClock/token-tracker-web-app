8bc64c2db2600f7f47b601a2fc7f6020a758b75e
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-02-11 00:36:36 -0500

    game token repository

---
+++ token-tracker-back-end/src/main/java/org/example/tokentrackerbackend/repositories/GameTokenRepository.java
@@ -0,0 +1,13 @@
+package org.example.tokentrackerbackend.repositories;
+
+import org.example.tokentrackerbackend.models.GameToken;
+import org.example.tokentrackerbackend.models.GameInstance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface GameTokenRepository extends JpaRepository < GameToken, Long > {
+    List < GameToken > findByGameInstance(GameInstance gameInstance);
+    List < GameToken > findByGameInstanceId(Long gameId);
+}
